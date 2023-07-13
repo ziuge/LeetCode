114b8197fd5a7114d1d1de6ab721bbec2d7bcc63
Jiwon Choi <79574342+ziuge@users.noreply.github.com>
2023-07-13 16:54:20 +0900

    Time: 82 ms (99.13%), Space: 14.3 MB (41.74%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.swift
+++ 0605-can-place-flowers/0605-can-place-flowers.swift
@@ -1,34 +1,13 @@
 class Solution {
     func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
+        
         var maxFlower = 0
-        var flowerbedSimul = flowerbed
+        var plant = [0] + flowerbed + [0]
         
-        if flowerbed.count == 1 {
-            if flowerbed[0] == 0 {
-                return n <= 1
-            } else {
-                return n < 1
-            }
-        }
-        
-        if flowerbed[0] == 0 && flowerbed[1] == 0 {
-            maxFlower += 1
-            flowerbedSimul[0] = 1
-        }
-        if flowerbedSimul[flowerbed.count - 1] == 0 && flowerbedSimul[flowerbed.count - 2] == 0 {
-            maxFlower += 1
-            flowerbedSimul[flowerbed.count - 1] = 1
-        }
-        
-        var i = 1
-        
-        while i < flowerbed.count - 2 {
-            if flowerbedSimul[i-1] == 0 && flowerbedSimul[i] == 0 && flowerbedSimul[i+1] == 0 {
-                flowerbedSimul[i] = 1
+        for i in 1...flowerbed.count {
+            if plant[i-1] == 0 && plant[i] == 0 && plant[i+1] == 0 {
+                plant[i] = 1
                 maxFlower += 1
-                i += 1
-            } else {
-                i += 1
             }
         }
         
